baeed14e8508b06a562e4668c06741697d18614a
Robin Gather <rgather11@gmail.com>
2022-11-24 14:03:15 +0100

    added kafka

---
+++ kafka-producer/kafkaadmin.py
@@ -0,0 +1,17 @@
+from kafka.admin import KafkaAdminClient, NewTopic
+
+
+def delete_topics(admin):
+    admin.delete_topics(topics=['songs'])
+
+
+def create_topics(admin, topic_list):
+    admin.create_topics(new_topics=topic_list, validate_only=False)
+
+
+if __name__ == '__main__':
+    admin_client = KafkaAdminClient(bootstrap_servers="34.147.0.237:9092",
+                                    client_id='Spotty')  # use your VM's external IP Here!
+    topic_list = [NewTopic(name="songs", num_partitions=1, replication_factor=1),
+                  NewTopic(name="popularity", num_partitions=1, replication_factor=1)]
+    create_topics(admin_client, topic_list)
---
+++ kafka-producer/producer.py
@@ -0,0 +1,31 @@
+from kafka import KafkaProducer
+
+
+def kafka_python_producer_sync(producer, msg, topic):
+    producer.send(topic, bytes(msg, encoding='utf-8'))
+    print("Sending " + msg)
+    producer.flush(timeout=60)
+
+
+def success(metadata):
+    print(metadata.topic)
+
+
+def error(exception):
+    print(exception)
+
+
+def kafka_python_producer_async(producer, msg, topic):
+    producer.send(topic, bytes(msg, encoding='utf-8')).add_callback(success).add_errback(error)
+    producer.flush()
+
+
+if __name__ == '__main__':
+    producer = KafkaProducer(bootstrap_servers='34.147.0.237:9092')  # use your VM's external IP Here!
+    with open('D:/Github Repo/de-data-architecture-processing/data/spotify-stream.txt', encoding="utf-8") as f:
+        lines = f.readlines()
+
+    for line in lines:
+        kafka_python_producer_sync(producer, line, 'songs')
+
+    f.close()
